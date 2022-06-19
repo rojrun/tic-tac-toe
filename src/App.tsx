1531fd9cf25e3162a9a21e3d82f0682391e90088
rojrun <runroj@gmail.com>
2022-06-18 19:51:24 -0700

    made conditional statements for game board

--- src/App.tsx
+++ src/App.tsx
@@ -6,48 +6,51 @@ import GameBoard from './components/GameBoard';
 import './App.css';
 
 const App = () => {
-  const [visible, setVisible] = useState(true);
-  const [playerCount, setPlayerCount] = useState(null);
-  const [playerOne, setPlayerOne] = useState("");
-  const [playerTwo, setPlayerTwo] = useState("");
-  const [isComputer, setIsComputer] = useState(false);
-  const [totalGameCount, setTotalGameCount] = useState(1);
-  const [xWins, setXWins] = useState(0);
-  const [oWins, setOWins] = useState(0);
-  // const [gameBoard, setGameBoard] = useState([]);
-
+  const [visible, setVisible] = useState<boolean>(true);
+  const [playerX, setPlayerX] = useState<string>("");
+  const [playerO, setPlayerO] = useState<string>("");
+  const [currentPlayer, setCurrentPlayer] = useState<string>("");
+  const [totalGameCount, setTotalGameCount] = useState<number>(1);
+  const [xWins, setXWins] = useState<number>(0);
+  const [oWins, setOWins] = useState<number>(0);
+  
   useEffect(() => {
-    console.log("playerCount: ", playerCount);
-    console.log("playerOne: ", playerOne);
-    console.log("playerTwo: ", playerTwo);
-    console.log("isComputer: ", isComputer);
-  }, [playerCount, playerOne, playerTwo, isComputer]);
+    console.log("currentPlayer: ", currentPlayer);
+    console.log("xWins: ", xWins);
+    console.log("oWins: ", oWins);
+  }, [currentPlayer, xWins, oWins]);
 
   return (
     <Container maxWidth="sm">
       <Paper variant="outlined">TIC TAC TOE</Paper>
       { visible &&
         <StartGame 
-          setPlayerCount={setPlayerCount} 
           setVisible={setVisible} 
-          setPlayerOne={setPlayerOne} 
-          setPlayerTwo={setPlayerTwo} 
-          setIsComputer={setIsComputer} 
+          setPlayerX={setPlayerX} 
+          setPlayerO={setPlayerO}
+          setCurrentPlayer={setCurrentPlayer}
         /> 
-      }  
+      }
       { !visible &&
         <>
           <Players 
-            playerOne={playerOne}
-            playerTwo={playerTwo} 
-            playerCount={playerCount} 
+            playerX={playerX}
+            playerO={playerO}
+            currentPlayer={currentPlayer} 
             xWins={xWins} 
             oWins={oWins} 
             totalGameCount={totalGameCount} 
-            setTotalGameCount={setTotalGameCount} 
-            isComputer={isComputer} 
           />
-          <GameBoard />
+          <GameBoard
+            currentPlayer={currentPlayer}
+            setCurrentPlayer={setCurrentPlayer}
+            xWins={xWins}
+            setXWins={setXWins}
+            oWins={oWins}
+            setOWins={setOWins}
+            totalGameCount={totalGameCount} 
+            setTotalGameCount={setTotalGameCount} 
+          />
         </>
       }
     </Container>

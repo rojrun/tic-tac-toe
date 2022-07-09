1e6f88740da19e9566a9f571a2c52be28b82c0f1
rojrun <runroj@gmail.com>
2022-07-08 20:56:36 -0700

    use resize addEventListener to calculate grid item

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect, useRef, useLayoutEffect} from 'react';
 import PlayAgainResetButtons from './PlayAgainResetButtons';
 import {Box, Paper, Grid} from '@mui/material';
 
@@ -46,19 +46,8 @@ const GameBoard = (
   const [gameBoard, setGameBoard] = useState<string[] | undefined>();
   const [showPlayAgainBttn, setShowPlayAgainBttn] = useState<boolean>(false);
   const [removeClick, setRemoveClick] = useState<boolean>(false);
-  const gridRefs = useRef<(HTMLDivElement | null)[]>([]);
-  const useRefDimension = (ref: React.RefObject<HTMLElement>) => {
-    const [elWidth, setElWidth] = useState<number>(1);
-    useEffect(() => {
-      if (ref.current) {
-        const square = ref.current[4].getBoundingClientRect();
-        console.log("square: ", square);
-        setElWidth(square);
-      }
-    }, [ref]);
-    return elWidth;
-  }
-  const widthDimension = useRefDimension(gridRefs);
+  const [elWidth, setElWidth] = useState<number>(1);
+  const gridRefs = useRef<(HTMLElement | null)[]>([]);
   const letter = (currentPlayer.split(" "))[1];
   
   useEffect(() => {
@@ -71,9 +60,12 @@ const GameBoard = (
   }, []);
 
   useEffect(() => {
+    console.log("currentPlayer: ", currentPlayer);
+    console.log('gameBoard: ', gameBoard);
     // Checks if currentPlayer is not empty & if the currentPlayer is "Computer"
     // Copy gameBoard and mark square with currentPlayer letter
-    if ( (currentPlayer !== "") && (currentPlayer.includes("Computer")) ) {
+    // if ( (currentPlayer !== "") && (currentPlayer.includes("Computer")) ) {
+    if (currentPlayer.includes("Computer")) {  
       const newGameBoard = [...[gameBoard]][0];
       
       // Checks if newGameBoard is not undefined
@@ -86,19 +78,27 @@ const GameBoard = (
         setTimeout(() => {
           setGameBoard(newGameBoard);
           checkForWinner();
-        }, 1500);
+        }, 2000);
       }
     } 
+  }, [currentPlayer, gameBoard]);
+
+  useEffect(() => {
+    getSquareWidth();
+  }, [gameBoard]);
+
+  useLayoutEffect(()=> {
+    window.addEventListener('resize', () => {
+      getSquareWidth();
+    });
   });
 
-  // useEffect(() => {
-  //   if (gridRefs.current.length === 9) {
-  //     // console.log("element 9 width: ", gridRefs.current[4].offsetWidth);
-  //     const el4Width = gridRefs.current[4]!.clientWidth - 8;
-  //     console.log("el4Width: ", el4Width);
-  //     setElWidth(el4Width);
-  //   }
-  // }, [gameBoard]);
+  const getSquareWidth = () => {
+    const square = gridRefs?.current[4]?.firstElementChild?.getBoundingClientRect()?.width;
+    if (square) {
+      setElWidth(square - 8);
+    }
+  }
 
   const handleMarkBox = (arrayIndex: number) => {
     const newGameBoard = [...[gameBoard]][0];
@@ -111,7 +111,7 @@ const GameBoard = (
     }
   }
 
-  const currentPlayerSetState = () => {
+  const swapPlayer = () => {
     if (currentPlayer.includes("X")) {
       setXWins(++xWins);
       setShowPlayAgainBttn(true);
@@ -128,39 +128,39 @@ const GameBoard = (
       // Horizontal lines
       case ( gameBoard && (gameBoard[0] === letter) && (gameBoard[1] === letter) && (gameBoard[2] === letter) ):
         setWinArray([0, 1, 2]);
-        currentPlayerSetState();
+        swapPlayer();
         break;
       case ( gameBoard && (gameBoard[3] === letter) && (gameBoard[4] === letter) && (gameBoard[5] === letter) ):
         setWinArray([3, 4, 5]);  
-        currentPlayerSetState();
+        swapPlayer();
         break;
       case ( gameBoard && (gameBoard[6] === letter) && (gameBoard[7] === letter) && (gameBoard[8] === letter) ):
         setWinArray([6, 7, 8]);
-        currentPlayerSetState();
+        swapPlayer();
         break;
         
       // Vertical lines
       case ( gameBoard && (gameBoard[0] === letter) && (gameBoard[3] === letter) && (gameBoard[6] === letter) ):
         setWinArray([0, 3, 6]);
-        currentPlayerSetState();
+        swapPlayer();
         break;
       case ( gameBoard && (gameBoard[1] === letter) && (gameBoard[4] === letter) && (gameBoard[7] === letter) ):
         setWinArray([1, 4, 7]);
-        currentPlayerSetState();
+        swapPlayer();
         break;
       case ( gameBoard && (gameBoard[2] === letter) && (gameBoard[5] === letter) && (gameBoard[8] === letter) ):
         setWinArray([2, 5, 8]);
-        currentPlayerSetState();
+        swapPlayer();
         break;
 
       // Diagonal lines
       case ( gameBoard && (gameBoard[0] === letter) && (gameBoard[4] === letter) && (gameBoard[8] === letter) ):
         setWinArray([0, 4, 8]);
-        currentPlayerSetState();
+        swapPlayer();
         break;
       case ( gameBoard && (gameBoard[2] === letter) && (gameBoard[4] === letter) && (gameBoard[6] === letter) ):
         setWinArray([2, 4, 6]);
-        currentPlayerSetState();
+        swapPlayer();
         break;
         
       // Else
@@ -190,11 +190,10 @@ const GameBoard = (
           { gameBoard && gameBoard.length > 0 ? (
               gameBoard.map((string, index) => {
                 return (
-                  <Grid item xs={4} key={index} onClick={() => !removeClick ? handleMarkBox(index) : null} ref={(el: HTMLDivElement) => (gridRefs.current.length !== gameBoard.length ? gridRefs.current.push(el) : null)}>
+                  <Grid item xs={4} key={index} onClick={() => !removeClick ? handleMarkBox(index) : null} ref={(el: HTMLDivElement) => (gridRefs.current?.length !== gameBoard.length ? gridRefs.current?.push(el) : null)}>
                     <Box
                       component="div"
-                      height={widthDimension}
-                      // height={gridRefs.current.length ? elWidth : 0}
+                      height={elWidth}
                     >
                       <span className={winArray.includes(index) ? "text_shadows" : ""}>{!string ? <>&nbsp;</> : string}</span>
                     </Box>

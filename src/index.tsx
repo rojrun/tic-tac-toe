4d33f9765fa2a3017693b71524ecfb7175a9bd80
rojrun <runroj@gmail.com>
2022-07-19 18:02:43 -0700

    removed redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import {store} from './store';
-import {Provider} from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function

24c1453eb7381ef2edc0d04774be7136eabd38b9
rojrun <runroj@gmail.com>
2022-08-10 13:16:30 -0700

    render start game test

--- src/_tests_/App.test.tsx
+++ src/_tests_/App.test.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from '../components/App';
 import StartGame from '../components/StartGame';
-import {StartGameProps} from '../types';
 
 describe("On load, render div with tic tac toe and how many players", () => {
   test("renders with h1 title", () => {
@@ -10,8 +9,30 @@ describe("On load, render div with tic tac toe and how many players", () => {
     const title = screen.getByText(/TIC TAC TOE/);
     expect(title).toBeInTheDocument();
   });
-
-  test("Start game component renders", () => {
-    render(<StartGame />);
-  });
 });
+
+describe("Render One or Two player buttons", () => {
+  test("Start game component renderings", () => {
+    const setVisible = jest.fn();
+    const setPlayerX = jest.fn();
+    const setPlayerO = jest.fn();
+    const setCurrentPlayer = jest.fn();
+
+    render(
+      <StartGame 
+        setVisible={setVisible} 
+        setPlayerX={setPlayerX} 
+        setPlayerO={setPlayerO}
+        setCurrentPlayer={setCurrentPlayer}
+      />
+    );
+    const onePlayerBttn = screen.getByTestId("onePlayer");
+    // const twoPlayersBttn = screen.getByTestId("twoPlayers");
+
+    fireEvent.click(onePlayerBttn);
+    const playerSelection = screen.getByText("Player, select X or O.");
+    expect(playerSelection).toBeInTheDocument();
+    
+  });
+})  
+
--- src/components/StartGame.tsx
+++ src/components/StartGame.tsx
@@ -42,8 +42,8 @@ const StartGame = (
       <Paper elevation={10} id="start_game" sx={{my: 2, py: 2, textAlign: "center"}}>
         <p>Welcome, is this a One or Two player game?</p>
         <ButtonGroup variant="contained" aria-label="outlined primary button group">
-          <Button onClick={handleOnePlayer}>One player</Button>
-          <Button onClick={handleTwoPlayers}>Two players</Button>
+          <Button onClick={handleOnePlayer} data-testid="onePlayer">One player</Button>
+          <Button onClick={handleTwoPlayers} data-testid="twoPlayers">Two players</Button>
         </ButtonGroup>
       </Paper>
       { isPlayerSelected
@@ -51,8 +51,8 @@ const StartGame = (
             <p>Player, select X or O.</p>
             <p>(X's go first)</p>
             <ButtonGroup variant="contained" aria-label="outlined primary button group">
-              <Button onClick={handleSelectX}>X</Button>
-              <Button onClick={handleSelectO}>O</Button>
+              <Button onClick={handleSelectX} data-testid="x">X</Button>
+              <Button onClick={handleSelectO} data-testid="o">O</Button>
             </ButtonGroup>
           </Paper>
         : null

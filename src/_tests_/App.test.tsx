b82a493f80b130510f526910bbb3cbaaeee596e7
rojrun <runroj@gmail.com>
2022-08-11 12:58:54 -0700

    add homepage to package.json for relative path deployment

--- src/_tests_/App.test.tsx
+++ src/_tests_/App.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from '../components/App';
 import StartGame from '../components/StartGame';
+import Players from '../components/Players';
 
 describe("On load, render div with tic tac toe and how many players", () => {
   test("renders with h1 title", () => {
@@ -26,12 +27,22 @@ describe("Render One or Two player buttons", () => {
         setCurrentPlayer={setCurrentPlayer}
       />
     );
-    const onePlayerBttn = screen.getByTestId("onePlayer");
-    // const twoPlayersBttn = screen.getByTestId("twoPlayers");
 
+    const onePlayerBttn = screen.getByTestId("onePlayer");
     fireEvent.click(onePlayerBttn);
     const playerSelection = screen.getByText("Player, select X or O.");
     expect(playerSelection).toBeInTheDocument();
+    
+    const xBttn = screen.getByTestId("x");
+    fireEvent.click(xBttn);
+    const view = render(<Players  playerX="Player X" playerO="Computer O" currentPlayer="Player X" xWins={0} oWins={0} totalGameCount={1} />);
+    // const playerXCard = view.container.querySelector("#player_x");
+    expect(view).toBeInTheDocument();
+    // const playerOCard = screen.getByText("Player 0");
+    // expect(playerOCard).toBeInTheDocument();
+
+
+    // const twoPlayersBttn = screen.getByTestId("twoPlayers");
     
   });
 })  
